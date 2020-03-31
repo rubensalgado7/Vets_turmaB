2bdd7fbbdfda85cf8ebbd0f99389ef07b68ccf89
Ruben Salgado <aluno21374@ipt.pt>
2020-03-31 15:33:24 +0100

    criaçao das classes que representam as tabelas da minha BD

---
+++ Vets/Vets/Models/Animais.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vets.Models
+{
+    public class Animais
+    {
+
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Especie { get; set; }
+
+        public string Raca { get; set; }
+
+        public double Peso { get; set; }
+
+        public string Foto { get; set; }
+    }
+}
